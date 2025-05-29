d7691106a4b3ec263d66f66d2e6779460ff9eb59
MercePalacios <merce.palacios-lopez@hotmail.com>
2025-05-29 17:35:08 +0200

    test

--- script.R
+++ script.R
@@ -2,4 +2,8 @@
 
 # Libraries for github installation
 library(usethis)
-library(credentials)
+library(gitcreds)
+
+use_git_config(user.name = "MercePalacios", user.email = "merce.palacios-lopez@hotmail.com")
+create_github_token()
+gitcreds_set()
